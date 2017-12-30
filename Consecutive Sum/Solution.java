56a6bbf00d2b198b5c7800aa908a788f7e257f98
Huili <huily02@gmail.com>
2017-12-30 21:24:09 +0800

    Uploaded java class file

---
+++ Consecutive Sum/Solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution{
+	
+	private static Scanner sc;
+
+	static int consecutive(long num){
+		int wayscount=0,sum=0,j=0;
+		
+		for(int i=1; i<num; i++) {
+			sum=i;
+			j=i+1;
+			
+			while(sum<num) {
+				sum = sum + j;
+				j++;
+			}
+			
+			if(sum == num) {
+				wayscount++;
+			}
+		}
+		
+		return wayscount;
+	}
+	
+	public static void main(String args[])throws IOException {
+	   sc = new Scanner(System.in);
+	   System.out.println("Enter a number : "); //inputting the number
+	   int n= sc.nextInt();
+	   
+	   System.out.println(consecutive(n));
+	}
+}

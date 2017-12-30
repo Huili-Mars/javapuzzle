046b2787c8cd8003eb529a14bdb557d582c5ec7e
Huili <huily02@gmail.com>
2017-12-30 23:22:44 +0800

    Uploaded java class file

---
+++ Largest Subset Sum/Solution.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	static Long[] maxSubsetSum(int[] k) {
+		Long[] results = new Long[k.length];
+		
+		for(int i=0; i<k.length; i++) {
+			results[i] = (long) 0;
+			for(int j=k[i]; j>0; j--) {
+				double divided = (double)k[i]/(double)j;
+				if(divided == Math.floor(divided)) {
+					results[i] += j;
+				}
+			}
+		}
+		
+		return results;
+	}
+	
+	public static void main(String[] args) throws IOException{
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please enter number of elements:");
+		int elem = sc.nextInt();
+		int[] k = new int[elem];
+		
+		for(int i=0; i<elem; i++) {
+			k[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		System.out.println(Arrays.toString(maxSubsetSum(k)));
+		
+	}
+
+}

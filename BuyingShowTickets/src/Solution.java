b030175ee3bdb9d26cbedadf39b34a1171406285
Huili <huily02@gmail.com>
2017-12-30 21:06:46 +0800

    First Commit

---
+++ BuyingShowTickets/src/Solution.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	
+	static long waitingTime(int[]tickets,int p) {
+		int waitingTime = 0;
+		
+		while(tickets[p] != 0) {
+			for(int i=0; i<tickets.length; i++) {
+				if(tickets[i] != 0) {
+					tickets[i]-=1;
+					waitingTime++;
+					if(tickets[p] == 0)
+						break;
+				}
+			}
+		}
+		
+		return waitingTime;
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		boolean bError = true;
+		
+		do {
+			try {
+				System.out.println("Please enter number of people in the queue.");
+				int totalqueue=sc.nextInt();
+				
+				int[] tickets = new int[totalqueue];
+				
+				for(int i=0; i<totalqueue; i++) {
+					tickets[i] = sc.nextInt();
+				}
+				
+				int jessepos = sc.nextInt();
+				sc.close();
+				
+				System.out.println("Waiting Time:"+waitingTime(tickets,jessepos));
+				bError=false;
+
+			}
+			catch(InputMismatchException inputMismatchException)
+		    {
+		        System.out.printf("Error! You must enter integers");
+		        bError = false;
+
+		    }
+		
+		}
+		while(bError);
+	
+		
+	}
+
+}

7b3cecb895f829198edbbe36f457f41e3e4a719f
Huili <huily02@gmail.com>
2018-01-11 14:52:31 +0800

    Updated

--- Find the Winner/Solution.java
+++ Find the Winner/Solution.java
@@ -3,9 +3,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Solution {
-	
-	public static Scanner sc;
-	
+
 	static String winner(int[] andrea, int[] maria, String s) {
 		String winner = "";
 		int scoreM=0;
@@ -13,14 +11,14 @@ public class Solution {
 		
 		for(int i=0; i<andrea.length; i++) {
 			
-			if(s.toUpperCase().equals("EVEN")) {
+			if(s.equals("EVEN")) {
 				
 				if(i % 2 == 0) {
 					scoreA += andrea[i] - maria[i];
 					scoreM += maria[i] - andrea[i];
 				}
 			}
-			else if(s.toUpperCase().equals("ODD")){
+			else if(s.equals("ODD")){
 				if(i % 2 == 1) {
 					scoreA += andrea[i] - maria[i];
 					scoreM += maria[i] - andrea[i];
@@ -37,27 +35,27 @@ public class Solution {
 	}
 
 	public static void main(String[] args) throws IOException{
-		sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
-		//System.out.println("Please enter number of elements in Andrea.");
 		int andrea = sc.nextInt();
 		int[] andreaDices = new int[andrea];
 		for(int i=0; i<andrea; i++) {
 			andreaDices[i] = sc.nextInt();
 		}
 		
-		//System.out.println("Please enter number of elements in Maria.");
 		int maria = sc.nextInt();
 		int[] mariaDices = new int[maria];
 		for(int i=0; i<maria; i++) {
 			mariaDices[i] = sc.nextInt();
 		}
 		
-		//System.out.println("Please indicate the game is even or odd.");
-		String gamemode = sc.next();
+		String gamemode = sc.next().toUpperCase();
 		
 		sc.close();
-		System.out.println("The winner is " + winner(andreaDices,mariaDices,gamemode));
+		
+		String gameWinner = winner(andreaDices,mariaDices,gamemode);
+		
+		System.out.println(gameWinner);
 	}
 
 }
